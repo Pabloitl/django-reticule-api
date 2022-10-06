bbd17f3ce753a1f37b25dc8bdc69d64ec0a8001d
Pablo <https://github.com/settings/emails>
2022-10-05 21:37:49 -0500

    feat(reticules): Include nested serializer subjects

--- reticules/serializers.py
+++ reticules/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
 from reticules.models import Reticule
+from subjects.serializers import SubjectSerializer
 
 
 class ReticuleSerializer(ModelSerializer):
+    subjects = SubjectSerializer(many=True, read_only=True)
+
     class Meta:
         model = Reticule
         fields = "__all__"
